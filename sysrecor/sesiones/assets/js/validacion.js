3ccfb9168c13de2d41b880c4987217c08c5869f0
JuanPablo <juan252000pablo@gmail.com>
2018-07-10 18:24:42 -0500

    como funcionara el crear usuario

---
+++ sysrecor/sesiones/assets/js/validacion.js
@@ -0,0 +1,23 @@
+function validar(){
+	var Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Sobre_Nombre, Número_de_Identidad, Número_de_Celular, Número_de_Telefono, Correro_Electronico, Contraseña, Expresion;
+	Primer_Nombre = document.getElementById('PrimerNombre').value;
+	Segundo_Nombre = document.getElementById('SegundoNombre').value;
+	Primer_Apellido = document.getElementById('PrimerApellido').value;
+	Segundo_Apellido = document.getElementById('SegundoApellido').value;
+	Sobre_Nombre = document.getElementById('SobreNombre').value;
+	Número_de_Identidad = document.getElementById('NúmerodeIdentidad').value;
+	Número_de_Celular = document.getElementById('NúmerodeCelular').value;
+	Número_de_Telefono = document.getElementById('NúmerodeTelefono').value;
+	Correro_Electronico = document.getElementById('CorreroElectronico').value;
+	Contraseña = document.getElementById('Contraseña').value;
+
+
+	if (Correro_Electronico === "" || ) {
+		alert("El campo nombre esta vacio");
+		return false;
+	}
+
+
+	}
+
+}
